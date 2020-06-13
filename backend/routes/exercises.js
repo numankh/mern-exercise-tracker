4d1ef7d28e29d4e9b8002b2764c5fa453e59d447
numankh <khannuman15@gmail.com>
2020-06-13 12:24:38 -0400

    finished CRUD for exercises

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,15 +3,14 @@ const router = require('express').Router();
 // importing the exercise schema
 let Exercise = require('../models/exercise.model');
 
-// endpoint for localhost:5000/exercises/ to return all exercises
+// return all exercises
 router.route('/').get((req, res) => {
     Exercise.find()
         .then(exercises => res.json(exercises))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// endpoint for localhost:5000/exercises/add to add a new exercise
-// given a username, description, duration, date
+// add a new exercise given a username, description, duration, date
 router.route('/add').post((req, res) => {
     const username = req.body.username;
     const description = req.body.description;
@@ -28,6 +27,36 @@ router.route('/add').post((req, res) => {
     newExercise.save()
     .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// ":id" is like a variable 
+// given an exercise id, return the exercise
+router.route('/:id').get((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => res.json(exercise))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// given an exercise id, delete the exercise
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete(req.params.id)
+        .then(() => res.json('Exercise deleted.'))
+        .catch(err => res.status(400).json('Error: ' + err))
+});
+
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.username = req.body.username;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+
+            exercise.save()
+                .then(() => res.json('Exercise updated!'))
+                .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
