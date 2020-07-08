7ed604cef9f0ff40aa99c9f34ed884282a6be841
numankh <khannuman15@gmail.com>
2020-07-07 20:17:49 -0400

    reorganized app.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Spinner from "react-bootstrap/Spinner";
 
-
-import Navbar from "./components/navbar"
-import ExercisesList from "./components/exercises-list"
-import CreateExercise from "./components/create-exercise"
-import CreateUser from "./components/create-user"
-import EditExercise from "./components/edit-exercise"
-
+import Navbar from "./components/navbar";
+import ExercisesList from "./components/exercises-list";
+import CreateExercise from "./components/create-exercise";
+import CreateUser from "./components/create-user";
+import EditExercise from "./components/edit-exercise";
 
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <br/>
-        <Switch>
-          <Route exact path='/' component={ExercisesList} />
-          <Route exact path='/create' component={CreateExercise} />
-          <Route exact path='/user' component={CreateUser} />
-          <Route exact path='/edit/:id' component={EditExercise} />
-        </Switch>
-      </div>
-    </Router>
+    <Suspense fallback={<Spinner animation="border" />}>
+      <Navbar />
+      <Router>
+        <div className="container">
+          <br />
+          <Switch>
+            <Route exact path="/" component={ExercisesList} />
+            <Route exact path="/create" component={CreateExercise} />
+            <Route exact path="/user" component={CreateUser} />
+            <Route exact path="/edit/:id" component={EditExercise} />
+          </Switch>
+        </div>
+      </Router>
+    </Suspense>
   );
 }
 
